6184ded3d993daaff848ea29be03f036df7f742e
maria <mariataktasheva@gmail.com>
2024-10-26 02:42:04 +0000

    eval script

--- full_eval.py
+++ full_eval.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -11,76 +11,81 @@
 
 import os
 from argparse import ArgumentParser
-import time
 
-mipnerf360_outdoor_scenes = ["bicycle", "flowers", "garden", "stump", "treehill"]
-mipnerf360_indoor_scenes = ["room", "counter", "kitchen", "bonsai"]
-tanks_and_temples_scenes = ["truck", "train"]
-deep_blending_scenes = ["drjohnson", "playroom"]
+mipnerf360_outdoor_scenes = ["garden"]
+mipnerf360_indoor_scenes = ["room"]
+scannetpp_scenes = [
+    "2e74812d00",
+    "0a7cc12c0e",
+    "0a76e06478",
+    "0cf2e9402d",
+    "0e75f3c4d9",
+    "1ae9e5d2a6",
+    "1b75758486",
+    "1c4b893630",
+    "4c5c60fa76",
+    "4ea827f5a1",
+    "6b40d1a939",
+    "5748ce6f01",
+    "7079b59642",
+]
 
 parser = ArgumentParser(description="Full evaluation script parameters")
 parser.add_argument("--skip_training", action="store_true")
 parser.add_argument("--skip_rendering", action="store_true")
 parser.add_argument("--skip_metrics", action="store_true")
 parser.add_argument("--output_path", default="./eval")
-parser.add_argument("--use_depth", action="store_true")
-parser.add_argument("--use_expcomp", action="store_true")
-parser.add_argument("--fast", action="store_true")
-parser.add_argument("--aa", action="store_true")
-
-
-
-
 args, _ = parser.parse_known_args()
 
 all_scenes = []
 all_scenes.extend(mipnerf360_outdoor_scenes)
 all_scenes.extend(mipnerf360_indoor_scenes)
-all_scenes.extend(tanks_and_temples_scenes)
-all_scenes.extend(deep_blending_scenes)
+all_scenes.extend(scannetpp_scenes)
 
 if not args.skip_training or not args.skip_rendering:
-    parser.add_argument('--mipnerf360', "-m360", required=True, type=str)
-    parser.add_argument("--tanksandtemples", "-tat", required=True, type=str)
-    parser.add_argument("--deepblending", "-db", required=True, type=str)
+    parser.add_argument("--mipnerf360", "-m360", type=str, default="/mnt/data/360_v2")
+    parser.add_argument(
+        "--scannetpp", "-spp", type=str, default="/mnt/data/scannetpp/data"
+    )
     args = parser.parse_args()
+
 if not args.skip_training:
-    common_args = " --disable_viewer --quiet --eval --test_iterations -1 "
-    
-    if args.aa:
-        common_args += " --antialiasing "
-    if args.use_depth:
-        common_args += " -d depths2/ "
-
-    if args.use_expcomp:
-        common_args += " --exposure_lr_init 0.001 --exposure_lr_final 0.0001 --exposure_lr_delay_steps 5000 --exposure_lr_delay_mult 0.001 --train_test_exp "
-
-    if args.fast:
-        common_args += " --optimizer_type sparse_adam "
-
-    start_time = time.time()
+    os.makedirs(args.output_path, exist_ok=True)
+    common_args = " --quiet --eval --test_iterations -1 --ip 127.0.0.20"
     for scene in mipnerf360_outdoor_scenes:
         source = args.mipnerf360 + "/" + scene
-        os.system("python train.py -s " + source + " -i images_4 -m " + args.output_path + "/" + scene + common_args)
+        os.system(
+            "python train.py -s "
+            + source
+            + " -i images_4 -m "
+            + args.output_path
+            + "/"
+            + scene
+            + common_args
+        )
     for scene in mipnerf360_indoor_scenes:
         source = args.mipnerf360 + "/" + scene
-        os.system("python train.py -s " + source + " -i images_2 -m " + args.output_path + "/" + scene + common_args)
-    m360_timing = (time.time() - start_time)/60.0
+        os.system(
+            "python train.py -s "
+            + source
+            + " -i images_2 -m "
+            + args.output_path
+            + "/"
+            + scene
+            + common_args
+        )
+    for scene in scannetpp_scenes:
+        source = args.scannetpp + "/" + scene
+        os.system(
+            "python train.py -s "
+            + source
+            + " -m "
+            + args.output_path
+            + "/"
+            + scene
+            + common_args
+        )
 
-    start_time = time.time()
-    for scene in tanks_and_temples_scenes:
-        source = args.tanksandtemples + "/" + scene
-        os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
-    tandt_timing = (time.time() - start_time)/60.0
-
-    start_time = time.time()
-    for scene in deep_blending_scenes:
-        source = args.deepblending + "/" + scene
-        os.system("python train.py -s " + source + " -m " + args.output_path + "/" + scene + common_args)
-    db_timing = (time.time() - start_time)/60.0
-
-with open(os.path.join(args.output_path,"timing.txt"), 'w') as file:
-    file.write(f"m360: {m360_timing} minutes \n tandt: {tandt_timing} minutes \n db: {db_timing} minutes\n")
 
 if not args.skip_rendering:
     all_sources = []
@@ -88,25 +93,33 @@ if not args.skip_rendering:
         all_sources.append(args.mipnerf360 + "/" + scene)
     for scene in mipnerf360_indoor_scenes:
         all_sources.append(args.mipnerf360 + "/" + scene)
-    for scene in tanks_and_temples_scenes:
-        all_sources.append(args.tanksandtemples + "/" + scene)
-    for scene in deep_blending_scenes:
-        all_sources.append(args.deepblending + "/" + scene)
-    
-    common_args = " --quiet --eval --skip_train"
-    
-    if args.aa:
-        common_args += " --antialiasing "
-    if args.use_expcomp:
-        common_args += " --train_test_exp "
+    for scene in scannetpp_scenes:
+        all_sources.append(args.scannetpp + "/" + scene)
 
+    common_args = " --quiet --eval --skip_train"
     for scene, source in zip(all_scenes, all_sources):
-        os.system("python render.py --iteration 7000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
-        os.system("python render.py --iteration 30000 -s " + source + " -m " + args.output_path + "/" + scene + common_args)
+        os.system(
+            "python render.py --iteration 7000 -s "
+            + source
+            + " -m "
+            + args.output_path
+            + "/"
+            + scene
+            + common_args
+        )
+        os.system(
+            "python render.py --iteration 30000 -s "
+            + source
+            + " -m "
+            + args.output_path
+            + "/"
+            + scene
+            + common_args
+        )
 
 if not args.skip_metrics:
     scenes_string = ""
     for scene in all_scenes:
-        scenes_string += "\"" + args.output_path + "/" + scene + "\" "
+        scenes_string += '"' + args.output_path + "/" + scene + '" '
 
-    os.system("python metrics.py -m " + scenes_string)
+    print("python metrics.py -m " + scenes_string)
